2b9821d8111f40574131d7ef97679c2437f40129
lordstarkthewolf <34372340+lordstarkthewolf@users.noreply.github.com>
2021-01-26 01:10:26 +0600

    Update Way Too Long Words(71A).c

--- Way Too Long Words(71A).c
+++ Way Too Long Words(71A).c
@@ -2,20 +2,20 @@
 #include<ctype.h>
 #include<string.h>
 int main(){
-    int n,m;
-char x[2000];
-scanf("%d",&n);
-for(int i=1;i<=n;i++){
-scanf("%s",&x);
-m=strlen(x);
+    int n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        int b;
+        char a[100];
+        scanf("%s",&a);
+        b=strlen(a);
+        if(b>10){
+            printf("%c%d%c\n",a[0],b-2,a[b-1]);
+        }
+        else{
+            printf("%s",a);
+        }
+    }
+    return 0;
 
-if(m>10){
-
-    printf("%c",x[0]);
-    printf("%d",m-2);
-    printf("%c\n",x[m-1]);
-
-}
-else printf("%s\n",x);}
-return 0;
 }
